6dda2ec99b29786961b22d27e365d6236ddcf6fd
Jack Vine <jackv949@gmail.com>
2016-02-02 20:57:52 +1030

    Start game camera drift fixed
    The camera doesn't drift from the origin when the game starts.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -14,6 +14,8 @@ public class CameraFollow : MonoBehaviour
         //If there is no target set, attempt to find the player
         if (!target)
             target = GameObject.FindWithTag("Player").transform;
+
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
     }
 
     //LateUpdate ensures camera moves within the same from as the target
